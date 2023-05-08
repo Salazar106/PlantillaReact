f8a091eba9d52add856dbd921ece8afb71acfa8a
Salazar106 <jdps106@hotmail.com>
2023-05-08 17:31:09 -0500

    se implemento a medias un componente para un botos de redes sociales

---
+++ src/components/Estilos/SocialButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialButton = (logo) =>{
+styles={
+    font: `italic 2.2em "Fira Sans", serif`,
+    // text-shadow: `2px 3px 3px #34353590`
+}
+    return (
+        <></>
+    )
+}
